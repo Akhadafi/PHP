2a53d5f532d0f94e4d9d144a2b553925fff65319
khadafi0 <khadafiar495@gamail.com>
2022-02-20 14:42:38 +0700

    Login

---
+++ siswa/14login/hapus.php
@@ -0,0 +1,20 @@
+<?php
+require 'functions.php';
+
+$nisn = $_GET["nisn"];
+
+if (hapus($nisn) > 0) {
+  echo "
+		<script>
+			alert('data berhasil dihapus!');
+			document.location.href = 'index.php';
+		</script>
+	";
+} else {
+  echo "
+		<script>
+			alert('data gagal ditambahkan!');
+			document.location.href = 'index.php';
+		</script>
+	";
+}
